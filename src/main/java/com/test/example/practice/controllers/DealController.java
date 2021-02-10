98f8d3218ec3fc65bdf5eabe2ff8553a3da744a2
Mark <pivonosovm@gmail.com>
2021-02-10 16:15:56 +0200

    Update DealController.java

--- src/main/java/com/test/example/practice/controllers/DealController.java
+++ src/main/java/com/test/example/practice/controllers/DealController.java
@@ -2,7 +2,6 @@ package com.test.example.practice.controllers;
 
 import com.test.example.practice.model.dtos.DealDto;
 import com.test.example.practice.model.dtos.RequestDealDto;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +21,3 @@ public class DealController {
     }
 
 }
-
-
